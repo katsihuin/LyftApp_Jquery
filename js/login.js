e4341c1163b84790545ebb6b4f54de5afa57ca9c
katsihuin <katsihuin@gmail.com>
2017-02-26 00:02:49 -0500

    changed selectors into jquery in sign-up form

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
 	//jQuery va aqui
     $('#commentName').on('input', function() {
         var inputName=$(this);
@@ -24,20 +24,41 @@ $(document).ready(function() {
         }
     });
 });
+*/
+
+$(document).ready(init);
+
+function init()
+{
+	$('#commentName').keyup(validateName);
+	$('#commentLastName').on('keyup', validateLastName);
+	$('#commentEmail').on('keyup', validateEmail);
+	$('#commentInfo').on('keyup', validateInfo);
+	$('#btnValidation').on('click', function(){
+		if(validateForm()==false)
+			alert("Los campos no estan validados");
+		else{
+			alert("los campos estan validados");
+		}
+	});
+	$('#btnGoBack').on('click', goBack);
+}
+
+function goBack()
+{
+	window.location="signup.html";
+}
 
 function validateForm()
 {
-    var valid = true;
-    
-    valid = valid && validateName();
+    var inputName=$('#commentName').val();
+    localStorage.setItem('Name', inputName);
 
-    valid = valid && validateLastName();
+  	var inputLastName=$('#commentLastName').val();
+  	localStorage.setItem('LastName',inputLastName);
 
-    valid = valid && validateEmail();
-
-    valid = valid && validateInfo();
-
-    return valid;
+  	var inputEmail=$('#commentEmail').val();
+  	localStorage.setItem('Email', inputEmail);
 
     if (!validateName() || !validateLastName() || !validateEmail() || !validateInfo())
 	{
@@ -47,87 +68,103 @@ function validateForm()
 	}	
 	else
 	{
-        var valid;
-        var sendForm=$('sendForm');
-      	sendForm.href="app.html";
 		jsShow("commentPrompt");
 		producePrompt("Formulario Validado Exitósamente", "commentPrompt", "green");
 	}
 }
 
-/* Muestra mensaje validación */
+/* Muestra mensaje validación*/
 function jsShow(id)
 {
-	$('#id').css({'display':'block'});
+	document.getElementById(id).style.display="block";
 }
 
-/* Oculta mensaje validación */
+/* Oculta mensaje validación*/
 function jsHide(id)
 {
-	$('#id').css({'display':'none'});
+	document.getElementById(id).style.display="none";
 }
+
 /* Envia Mensaje al usuario*/
 function producePrompt(message, promptLocation, color)
 {
-	$('promptLocation').html = message;
-	$('promptLocation').css({'color':'color'});
-    
+	document.getElementById(promptLocation).innerHTML = message;
+	document.getElementById(promptLocation).style.color = color;
 }
 
-function firstToUpperCase(_texto)
+function firstToUpperCase()
 {
-	var result = _texto[0].toUpperCase() + _texto.slice(1);
-	var mayus = result.split(" ");
-	return result;
-}
+	$(this[0]).keyup(function(event) {
+        var box = event.target;
+        var txt = $(this).val();
+        var stringStart = box.selectionStart;
+        var stringEnd = box.selectionEnd;
+        $(this).val(txt.replace(/^(.)|(\s|\-)(.)/g, function($word) {
+            return $word.toUpperCase();
+        }));
+        box.setSelectionRange(stringStart , stringEnd);
+    });
 
+   return this;
+}
 
 /* Valida Nombre*/
 function validateName()
 {
-	var inputName = ($'commentName');
+	var inputName = $('#commentName');
 	var name =  inputName.val;
 
 	inputName.val = firstToUpperCase(name);
-
-	if (name.length == 0)
+	var nameReg = /^[A-Z][a-z]*[a-zA-Z]$/;
+	
+	if (name == null || name.length == 0 || /^\^s+$/.test(name))
 	{
 		producePrompt("Tu Nombre es requerido", "commentNamePrompt", "red");
+		inputName.removeClass("valid").addClass("invalid");
 		return false;
 	}
-	else if (!name.match(/^[A-Z][a-z]*[a-zA-Z]$/)) 
+	else if (!nameReg.test(name)) 
 	{
 		producePrompt("Compruebe que su Nombre contenga SOLO caracteres de la A-Z", "commentNamePrompt", "red");
+		inputName.removeClass("valid").addClass("invalid");
 		return false;
 	}
 	else 
 	{
 		producePrompt("Bienvenido(a) " + name, "commentNamePrompt", "green")
+		inputName.removeClass("invalid").addClass("valid");
+		return true;
 	}	
+
 
 }
 
 /* Valida Apellido*/
 function validateLastName()
 {
-	var inputLastName = document.getElementById("commentLastName");
-	var lastName =  inputLastName.value;
+	var inputLastName = $('#commentLastName');
+	var lastName =  inputLastName.val;
 
-	inputLastName.value = firstToUpperCase(lastName);
+	inputLastName.val = firstToUpperCase(lastName);
 
-	if (lastName.length == 0)
+	var lastNameReg = /^[A-Z][a-z]*[a-zA-Z]$/;
+
+	if (lastName == null || lastName.length == 0 || /^\^s+$/.test(lastName))
 	{
 		producePrompt("Tu Apellido es requerido", "commentLastNamePrompt", "red");
+		inputLastName.removeClass("valid").addClass("invalid");
 		return false;
 	}
-	else if (!lastName.match(/^[A-Z][a-z]*[a-zA-Z]$/)) 
+	else if (!lastNameReg.test(lastName)) 
 	{
 		producePrompt("Compruebe que su Apellido contenga SOLO caracteres de la A-Z", "commentLastNamePrompt", "red");
+		inputLastName.removeClass("valid").addClass("invalid");
 		return false;
 	}
 	else 
 	{
 		producePrompt("Apellido Válido ✔", "commentLastNamePrompt", "green");
+		inputName.removeClass("invalid").addClass("valid");
 		return true;
 	}	
 }
@@ -135,20 +172,26 @@ function validateLastName()
 /* Valida Email*/
 function validateEmail()
 {
-	var email = document.getElementById("commentEmail").value;
-	if (email.length == 0)
+	var inputEmail = $('#commentEmail');
+	var email = inputEmail.val;
+	var emailReg = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
+
+	if (email == null || email.length == 0 || /^\^s+$/.test(email))
 	{
 		producePrompt("Correo Electrónico es requerido", "commentEmailPrompt", "red");
+		inputEmail.removeClass("valid").addClass("invalid");
 		return false;
 	}
-	else if (!email.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) 
+	else if (!emailReg.test(email)) 
 	{
 		producePrompt("Compruebe que el Correo Electrónico contenga un formato válido. Ej: name@domain.com", "commentEmailPrompt", "red");
+		inputEmail.removeClass("valid").addClass("invalid");
 		return false;
 	}
 	else 
 	{
 		producePrompt("Correo Electrónico Válido ✔", "commentEmailPrompt", "green");
+		inputEmail.removeClass("invalid").addClass("valid");
 		return true;
 	}
 }
@@ -156,7 +199,7 @@ function validateEmail()
 /* Valida Terminos */
 function validateInfo()
 {
-	var info = document.getElementById("commentInfo");
+	var info = $('#commentInfo');
 	if (info.checked=true) 
 	{
 		producePrompt("¡Gracias por aceptar los Terminos de Usuario!", "commentInfoPrompt", "green");
